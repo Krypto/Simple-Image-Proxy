22570689ccad70f84549ef12bdf4dd2cc8cae9ac
darkalchemy <darkalchemy@jbmatrix.net>
2018-02-02 21:52:48 +0000

    update: response

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ define('ROOT_DIR', dirname(__FILE__, 2));
 require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'functions.php';
 
 if (!empty($_GET)) {
-    print_r(decrypt_url($_GET['url']));
+    echo decrypt_url($_GET['url']);
 } else {
     echo 'Invalid information supplied';
 }
